e948b55a33c9bfabe1a0e6bcc3abf207728c8065
onoah51 <57455201+onoah51@users.noreply.github.com>
2024-05-24 15:19:58 -0500

    Add files via upload

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,120 @@
+# Test connection to GitHub
+$canConnectToGithub = Test-Connection github.com -Count 1 -Quiet -TimeoutSeconds 1
+
+# Verify module installations
+if (-Not(Get-Module -ListAvailable -Name Terminal-Icons)) {
+    Install-Module -Name Terminal-Icons -Scope CurrentUser -Force -SkipPublisherCheck
+}
+if (-Not(Get-Module -ListAvailable -Name ActiveDirectory)) {
+    Install-Module -Name ActiveDirectory -Scope CurrentUser -Force -SkipPublisherCheck
+}
+if (-Not(Get-Module -ListAvailable -Name SnipeITPS)) {
+    Install-Module -Name SnipeITPS -Scope CurrentUser -Force -SkipPublisherCheck
+}
+
+# Import modules
+Import-Module -Name Terminal-Icons
+Import-Module -Name ActiveDirectory
+Import-Module -Name SnipeITPS
+
+# Function for profile updates
+function Update-Profile {
+    if (-Not $global:canConnectToGithub) {
+        Write-Host "Skipping profile update check due to GitHub not responding within 1 second." -ForegroundColor Yellow
+        Return
+    }
+
+    try {
+        $url = "https://raw.githubusercontent.com/onoah51/powershell-profile/main/Microsoft.PowerShell_profile.ps1"
+        $oldHash = Get-FileHash $Profile
+        Invoke-RestMethod $url -OutFile "$env:temp/Microsoft.PowerShell_profile.ps1"
+        $newHash = Get-FileHash "$env:temp/Microsoft.PowerShell_profile.ps1"
+        if ($newhash.Hash -ne $oldhash.Hash) {
+            Copy-Item -Path "$env:temp/Microsoft.PowerShell_profile.ps1" -Destination $PROFILE -Force
+            Write-Host "Profile has been updated. Please restart your shell to reflect changes." -ForegrounColor Magenta
+        }
+    } catch {
+        Write-Error "Unable to check for `$profile updates"
+    } finally {
+        Remove-Item "$env:temp/Microsoft.PowerShell_profile.ps1" -ErrorAction SilentlyContinue
+    }
+}
+
+function Update-PowerShell {
+    if (-Not $global:canConnectToGoogle) {
+        Write-Host "Skipping PowerShell update check due to GitHub not responding within 1 second." -ForegroundColor Yellow
+        Return
+    }
+
+    try {
+        Write-Host "Checking for PowerShell updates..." -ForegrounColor Cyan
+        $updateNeeded = $false
+        $currentVersion = $PSVersionTable.PSVersion.ToString()
+        $gitHubApiUrl = "https://api.github.com/repos/PowerShell/PowerShell/releases/latest"
+        $latestReleaseInfo = Invoke-RestMethod -Uri $gitHubApiUrl
+        $latestVersion = $latestReleaseInfo.tag_name.Trim('v')
+        if ($currentVersion -lt $latestVersion) {
+            $updateNeeded = $true
+        }
+
+        if ($updateNeeded) {
+            Write-Host "Updating PowerShell..." -ForegroundColor Yellow
+            winget upgrade "Microsoft.PowerShell" --accept-source-agreements --accept-package-agreements
+            Write-Host "PowerShell has been updated. Please restart your shell to reflect the changes." -ForegroundColor Magenta
+        } else {
+            Write-Host "Your PowerShell version is up to date." -ForegroundColor Green
+        }
+    } catch {
+        Write-Error "Failed to update PowerShell. Error: $_"
+    }
+}
+Update-PowerShell
+
+# Function to check for public IP address
+function Get-PubIP {
+    (Invoke-WebRequest http://ifconfig.me/ip).Content
+}
+
+# Function to find the computer's uptime
+function Get-Uptime {
+    if ($PSVersionTable.PSVersion.Major -eq 5) {
+        Get-CimInstance Win32_OperatingSystem | Select-Object @{Name='LastBootUpTime'; Expression={$_.ConverttoDateTime($_.lastbootuptime)}} | Format-Table -HideTableHeaders
+    } else {
+        net statistics workstation | Select-String "since" | ForEach-Object { $_.ToString().Replace('Statistics since ', '') }
+    }
+}
+
+# Function to reload your profile
+function Reload-Profile {
+    & $profile
+}
+
+function unzip ($file) {
+    Write-Output("Extracting", $file, "to", $pwd)
+    $fullFile = Get-ChildItem -Path $pwd -Filter $file | ForEach-Object { $_.FullName }
+    Expand-Archive -Path $fullFile -DestinationPath $pwd
+}
+
+# Enhanced PowerShell Experience
+Set-PSReadLineOption -Colors @{
+    Command = 'Yellow'
+    Parameter = 'Green'
+    String = 'DarkCyan'
+}
+
+# Set the prompt styling
+oh-my-posh init pwsh --config https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/atomicBit.omp.json | Invoke-Expression
+
+# Verify zioxide installation
+if (Get-Command zoxide -ErrorAction SilentlyContinue) {
+    Invoke-Expression (& { (zoxide init powershell | Out-String) })
+} else {
+    Write-Host "zoxide command not found. Attempting to install via winget..."
+    try {
+        winget install -e --id ajeetdsouza.zoxide
+        Write-Host "zoxide installed successfully. Initializing..."
+        Invoke-Expression (& { (zoxide init powershell | Out-String) })
+    } catch {
+        Write-Error "Failed to install zoxide. Error: $_"
+    }
+}
